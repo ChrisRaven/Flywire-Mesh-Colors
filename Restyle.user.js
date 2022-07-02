f143f022203d3b1bc3abd1bda02ff3e1eb899203
KrzysztofKruk <kr.kruk@gmail.com>
2022-07-02 23:43:24 +0200

    New way of creating Dock

--- Restyle.user.js
+++ Restyle.user.js
@@ -12,11 +12,14 @@
 // ==/UserScript==
 
 (() => {
-  if (globalThis.dockIsReady) return main()
+  // if (globalThis.dockIsReady) return main()
 
-  let script = document.createElement('script')
-  script.src = typeof DEV !== 'undefined' ? 'http://127.0.0.1:5501/FlyWire-Dock/Dock.js' : 'https://chrisraven.github.io/FlyWire-Dock/Dock.js'
-  document.head.appendChild(script)
+  if (!document.getElementById('dock-script')) {
+    let script = document.createElement('script')
+    script.id = 'dock-script'
+    script.src = typeof DEV !== 'undefined' ? 'http://127.0.0.1:5501/FlyWire-Dock/Dock.js' : 'https://chrisraven.github.io/FlyWire-Dock/Dock.js'
+    document.head.appendChild(script)
+  }
 
   let wait = setInterval(() => {
     if (globalThis.dockIsReady) {
