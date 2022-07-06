c51ab50b1c6faf86c40fe7d0f69c572cf65c75e1
KrzysztofKruk <kr.kruk@gmail.com>
2022-07-06 13:31:50 +0200

    Remove unnecessary comment and IIFE

--- Restyle.user.js
+++ Restyle.user.js
@@ -11,23 +11,19 @@
 // @homepageURL  https://github.com/ChrisRaven/FlyWire-Restyle
 // ==/UserScript==
 
-(() => {
-  // if (globalThis.dockIsReady) return main()
+if (!document.getElementById('dock-script')) {
+  let script = document.createElement('script')
+  script.id = 'dock-script'
+  script.src = typeof DEV !== 'undefined' ? 'http://127.0.0.1:5501/FlyWire-Dock/Dock.js' : 'https://chrisraven.github.io/FlyWire-Dock/Dock.js'
+  document.head.appendChild(script)
+}
 
-  if (!document.getElementById('dock-script')) {
-    let script = document.createElement('script')
-    script.id = 'dock-script'
-    script.src = typeof DEV !== 'undefined' ? 'http://127.0.0.1:5501/FlyWire-Dock/Dock.js' : 'https://chrisraven.github.io/FlyWire-Dock/Dock.js'
-    document.head.appendChild(script)
+let wait = setInterval(() => {
+  if (globalThis.dockIsReady) {
+    clearInterval(wait)
+    main()
   }
-
-  let wait = setInterval(() => {
-    if (globalThis.dockIsReady) {
-      clearInterval(wait)
-      main()
-    }
-  }, 100)
-})()
+}, 100)
 
 
 function main() {
